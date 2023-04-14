a94c4fc55f7b9d2fcbe942cd48efcaf3fc286f45
JuanHoyos <hoyospiedrahitajuanmanuel@gmail.com>
2023-04-14 11:43:42 -0500

    2

---
+++ 2-print_alphabet_x10.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+void print_alphabet_x10(void){
+    int a,b;
+    for(a = 1; a <= 10; a++){      
+        for(b = 'a'; b <= 'z';b++){
+            _putchar(b);
+        }
+        _putchar('\n');
+    }
+}  
